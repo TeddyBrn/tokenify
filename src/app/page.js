c69be080efb9db53cdc64d825567c33114cde8d4
Teddy <teddy.buran33@gmail.com>
2025-03-26 15:32:23 +0100

    .env fix

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,18 @@ import Image from 'next/image';
 import { PlaceholdersAndVanishInput } from '@/components/ui/placeholders-and-vanish-input';
 import { Roboto } from 'next/font/google';
 import { useState, useEffect } from 'react';
+require('dotenv').config();
 
 const roboto = Roboto({ subsets: ['latin'] });
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const CLIENT_ID = '3c8858e88a074034a41ac56d973c7ced';
-// const CLIENT_SECRET = '5bbcda3d2ea4430cba2002449ebe0578';
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET;
 
 export default function Home() {
   const placeholders = ['Search for an artist', 'Search for an artist'];
 
   const [searchInput, setSearchInput] = useState('');
 	const [accessToken, setAccessToken] = useState('');
-
+	
   useEffect(() => {
     // API Access Token
     let authParameters = {
