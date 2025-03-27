9a1a2905577ead6e0f521052b452fa3229f06b2a
Teddy <teddy.buran33@gmail.com>
2025-03-27 20:47:51 +0100

    welcome section responsive

--- src/components/ui/welcome-section.jsx
+++ src/components/ui/welcome-section.jsx
@@ -6,11 +6,11 @@ export function WelcomeSection() {
   return (
     <div className="flex flex-col items-center justify-center gap-6">
       <GradualSpacing
-        className="font-display text-center text-4xl font-bold -tracking-widest mb-8 text-[#39D66E] dark:text-white md:text-8xl md:leading-[5rem]"
+        className="font-display text-center text-4xl md:text-7xl font-bold -tracking-widest mb-8 text-[#39D66E] dark:text-white lg:text-8xl md:leading-[5rem]"
         text="What is Tokenify ?"
       />
       <FadeText
-        className="text-6xl sm:2xl font-bold text-[#BABABA] dark:text-white"
+        className="text-3xl md:text-5xl lg:text-6xl sm:2xl font-bold text-[#BABABA] dark:text-white"
         direction="up"
         framerProps={{
           show: { transition: { delay: 1 } }
@@ -18,7 +18,7 @@ export function WelcomeSection() {
         text="Find an artist"
       />
       <FadeText
-        className="text-6xl font-bold text-[#BABABA] dark:text-white"
+        className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#BABABA] dark:text-white"
         direction="up"
         framerProps={{
           show: { transition: { delay: 2 } }
@@ -26,7 +26,7 @@ export function WelcomeSection() {
         text="Their best songs"
       />
       <FadeText
-        className="text-6xl font-bold text-[#BABABA] dark:text-white"
+        className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#BABABA] dark:text-white"
         direction="up"
         framerProps={{
           show: { transition: { delay: 3 } }
